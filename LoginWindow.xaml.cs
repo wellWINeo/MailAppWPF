1841a8d11361d6d1de2d22a30311653e5b41c51f
wellWINeo <usa200313@gmail.com>
2021-04-12 21:32:57 +0300

    init commit

---
+++ LoginWindow.xaml.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace MailApp
+{
+    /// <summary>
+    /// Логика взаимодействия для LoginWindow.xaml
+    /// </summary>
+    public partial class LoginWindow : Window
+    {
+        public string Login, Passwd;
+
+        public LoginWindow()
+        {
+            InitializeComponent();
+        }
+
+        public void LoginClick(object button, RoutedEventArgs e)
+        {
+            this.Login = LoginBox.Text;
+            this.Passwd = PasswdBox.Password;
+            this.Close();
+        }
+    }
+}
